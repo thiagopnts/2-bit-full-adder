3f22a296c1166d20044f5496d4194147a5d1da4a
Thiago Pontes <thiagopnts@gmail.com>
2011-10-18 12:36:52 -0300

    Simplified the sum

--- Makefile
+++ Makefile
@@ -3,5 +3,5 @@ all: SYSTEMC
 	@./main
 
 SYSTEMC:
-	@g++ -c and.h xor.h stim.h mon.h or2.h adder2.h -m32 -I/Library/SystemC/Current/include -L/Library/SystemC/Current/lib-macosx
+	@g++ -c stim.h mon.h adder2.h -m32 -I/Library/SystemC/Current/include -L/Library/SystemC/Current/lib-macosx
 
--- mon.h
+++ mon.h
@@ -7,18 +7,21 @@ SC_MODULE(mon) {
     sc_in<bool> A1,A2,B1,B2,Cin, Sum1, Sum2, Cout, Clk;
 
     void monitor() {
-        cout << setw(10) << "Time";
         cout << setw(4) << "A1";
-        cout << setw(4) << "B1";
+        cout << setw(3) << "A2";
+        cout << setw(3) << "B1";
+        cout << setw(3) << "B2";
         cout << setw(5) << "Cout";
-        cout << setw(3) << "Sum" << endl;
-
+        cout << setw(3) << "S1";
+        cout << setw(3) << "S2";
         while(true) {
-            cout << setw(10) << sc_time_stamp();
-            cout << setw(3) << A1.read() << A2.read();
-            cout << setw(3) << B1.read() << B2.read();
+            cout << setw(3) << A1.read();
+            cout << setw(3) << A2.read();
+            cout << setw(3) << B1.read();
+            cout << setw(3) << B2.read();
             cout << setw(5) << Cout.read();
-            cout << Sum1.read() << Sum2.read() << endl;
+            cout << setw(3) << Sum1.read(); 
+            cout << setw(4) << Sum2.read() << endl;
             wait();
         }
     }
